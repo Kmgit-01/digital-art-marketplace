25fefce5156174caf40598f7b6f4602907e0d241
Ubuntu <azureuser@art-marketplace-vm.l4vu2cmmhieetevaex4ljgx3ud.rx.internal.cloudapp.net>
2026-07-07 20:59:15 +0000

    Serve frontend static build from Express for VM deployment

--- server/index.js
+++ server/index.js
@@ -9,14 +9,19 @@ const transactionRoutes = require('./routes/transactions');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('../client/dist'));
 
-app.get('/', (req, res) => {
+app.get('/api/health-check', (req, res) => {
   res.send('Digital Art Marketplace API is running');
 });
 
 app.use('/api/artworks', artworkRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/transactions', transactionRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: '../client/dist' });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
